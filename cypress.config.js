f657f53b704adf4f7d2ebb200cb8f43ebbb777b9
Pietro Souza Cardoso <49520791+Pietro19@users.noreply.github.com>
2026-02-11 21:07:10 -0300

    add page Playwright

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,11 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+        
+      
     },
   },
 };
 
 
-export default defineConfig({
-  video: false,
-  numTestsKeptInMemory: 0,
 
-  e2e: {
-    experimentalMemoryManagement: true
-  }
-})
